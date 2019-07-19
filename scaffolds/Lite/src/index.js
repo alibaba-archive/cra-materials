8769bcace2fc95d057001dc5b18d37dc00b3cf7b
tanhengyeow <E0032242@u.nus.edu>
2019-07-19 16:32:49 +0800

    chore: Update PR
    - Admin for package.json
    - Add Service Worker files to scaffold

--- scaffolds/Lite/src/index.js
+++ scaffolds/Lite/src/index.js
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom';
 import router from './router';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(router(), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
