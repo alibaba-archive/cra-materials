611d634abe8bed9840c79ecce30e36b8a5e0259c
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2019-07-29 19:24:55 +0800

    chore: add upload scripts

---
+++ scripts/upload-oss.js
@@ -0,0 +1,70 @@
+/* eslint arrow-body-style:0 */
+const oss = require('ali-oss');
+const path = require('path');
+const fs = require('fs');
+const scaffolds = require('./scaffolds');
+const pkgData = require('../package.json');
+
+const bucket = 'iceworks';
+const accessKeyId = process.env.ACCESS_KEY_ID;
+const accessKeySecret = process.env.ACCESS_KEY_SECRET;
+const branch = process.env.TRAVIS_BRANCH;
+const dirPath = pkgData.kitVersion === '3.x' ? 'materials/' : '/';
+let assetsPath;
+
+if (['master', 'stable/kit-2.x'].indexOf(branch) === -1) {
+  assetsPath = 'pre-assets';
+} else {
+  assetsPath = 'assets';
+}
+
+const ossClient = oss({
+  bucket,
+  endpoint: 'oss-cn-hangzhou.aliyuncs.com',
+  accessKeyId,
+  accessKeySecret,
+  time: '120s',
+});
+
+const materialPath = path.resolve(__dirname, '../build/materials.json');
+const toPath = path.join(assetsPath, dirPath, 'cra-materials.json');
+
+/**
+ * 按照下载量进行排序推荐
+ */
+function sortScaffoldMaterials() {
+  return new Promise((resolve, reject) => {
+    const materialsData = JSON.parse(fs.readFileSync(materialPath, 'utf-8'));
+
+    const sortMaterialsData = [];
+    scaffolds.forEach((scaffold) => {
+      materialsData.scaffolds.forEach((currentItem) => {
+        if (currentItem.name === scaffold) {
+          sortMaterialsData.push(currentItem);
+        }
+      });
+    });
+
+    materialsData.scaffolds = sortMaterialsData;
+
+    return fs.writeFile(
+      materialPath,
+      JSON.stringify(materialsData, null, 2),
+      'utf-8',
+      (err) => {
+        if (err) reject(err);
+        resolve();
+      },
+    );
+  });
+}
+
+console.log('start upload oss', materialPath, toPath);
+
+sortScaffoldMaterials()
+  .then(() => {
+    return ossClient.put(toPath, materialPath);
+  })
+  .then((result) => {
+    console.log('upload success', result);
+  });
